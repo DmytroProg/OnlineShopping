2a165f2ed4475421bb05a9dc2c5c6c2d567400b0
DmytroProg <dmytroprog@gmail.com>
2025-01-06 18:27:28 +0200

    Add project files.

---
+++ OnlineShopping/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace OnlineShopping.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+    public string Email { get; set; }
+    public string? Phone { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+    public DateTime? DeletedAt { get; set; } // soft delete
+}
